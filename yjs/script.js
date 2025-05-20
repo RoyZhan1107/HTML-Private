14a68cc0d26d0253afb20e6a3deb13c844f37765
RoyZhan1107 <142411359+RoyZhan1107@users.noreply.github.com>
2025-05-20 08:56:34 +0800

    Add files via upload

--- yjs/script.js
+++ yjs/script.js
@@ -1,6 +1,11 @@
+// auto play video
 window.onload = function() {
     var video = document.getElementById('video');
     video.play().catch(error =>{
         console.log("Auto play failed: ", error);
     });
 };
+// limit right button
+document.addEventListener("contextmenu", function(event){
+    event.preventDefault();
+})
